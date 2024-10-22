5fc33619e2269f4e143b8f15f43ab8af877c6024
SonicPika <koteykaandchernishka@gmail.com>
2024-10-22 13:34:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('shakeButton').addEventListener('click', function() {
-    const elements = document.querySelectorAll('main, .selfie, .left-image, aside, section');
-    elements.forEach(element => {
-        element.classList.toggle('shake');
-        element.classList.toggle('spin');
-    });
-});
