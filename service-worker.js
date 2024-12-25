d6433406c46291280ba2c65fa67679f98856dc99
SonicPika <sonicpika@duck.com>
2024-12-25 04:56:34 +0100

    servicenigga

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('push', event => {
+    const options = {
+        body: 'NIGGERS ARE SO SEXY - BUY YOURS NOW AT NIGGER-SEX.XYZ/SIGGERS',
+    };
+    event.waitUntil(self.registration.showNotification('analsex', options));
+});
